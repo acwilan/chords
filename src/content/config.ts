60ff065dba7466aa9787593a3ad7658876c78b2b
Andres Rovira <wrovira@paypal.com>
2025-09-07 20:36:41 -0600

    Define songs content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
-const examples = defineCollection({
+const songs = defineCollection({
   schema: z.object({
-    title: z.string()
-  })
+    title: z.string(),
+    key: z.string().optional(),
+    bpm: z.number().optional(),
+    capo: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    pdf: z.string().optional(),
+  }),
 });
 
-export const collections = { examples };
+export const collections = { songs };
+
