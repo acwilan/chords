927c6b0c59f1f1d2d8057d527c98139f7aca8b3e
Andres Rovira <andres.rovira@gmail.com>
2025-09-07 20:33:37 -0600

    config: set site and base for deployment (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 // https://docs.astro.build/en/reference/configuration-reference/
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://acwilan.github.io',
+  base: '/chords',
+});
