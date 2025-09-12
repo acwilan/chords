4a4915e659d4ca2bb492134cc831c6ac8ae91899
Andres Rovira <andres.rovira@gmail.com>
2025-09-11 20:54:40 -0600

    chore: rename project to ChordBook (#52)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-  site: 'https://acwilan.github.io',
-  base: '/chords',
+  site: "https://acwilan.github.io",
+  base: '/chordbook',
   integrations: [tailwind()],
 });
